0e098f8cf9b57c9591da8d8f540f22cb6d8dadc4
chernyhleb <popovarlol@gmail.com>
2020-03-19 21:44:15 +0000

    added ex2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,25 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int len, i;
+    char *beg, *end, temp;
+
+    i = 0; 
+    while( *(str+i) != '\0' )
+       i++;
+    len = i;
+
+    beg = str;
+    end = str + len - 1;
+
+    for ( i = 0 ; i < len/2 ; i++ )
+    {
+       temp = *end;
+       *end = *beg;
+       *beg = temp;
+
+       beg++;
+       end--;
+    }
 }
 
