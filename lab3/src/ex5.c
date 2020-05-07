216f6ca1ea803ce51f54cb87b1e26d9243070afd
chernyhleb <popovarlol@gmail.com>
2020-05-07 10:32:51 +0000

    ex 5 is finished

---
+++ lab3/src/ex5.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <signal.h>
+
+int main(int argc, char **argv)
+{
+    int pid = fork();
+    if(pid == 0)
+    {
+        printf("fork success!");
+        execv("sequential_min_max", argv);
+    }
+    else 
+    if(pid < 0) 
+    {
+        printf("Fork failed!");
+        return -1;
+    }
+
+    return 0;
+}
