b1fba131e2c16656af7f56938ee747fd07b3b922
chernyhleb <popovarlol@gmail.com>
2020-05-09 13:13:09 +0000

    ex5 is finished

---
+++ lab4/src/arr_sum.c
@@ -0,0 +1,16 @@
+#include "arr_sum.h"
+
+struct SumArgs {
+  int *array;
+  int begin;
+  int end;
+};
+
+int Sum(const struct SumArgs *args) {
+  int sum = 0;
+  for (int i = args->begin; i < args->end; i++)
+  {
+    sum+=args->array[i];
+  } 
+  return sum;
+}
---
+++ lab4/src/arr_sum.h
@@ -0,0 +1,8 @@
+#ifndef ARR_SUM_H
+#define ARR_SUM_H
+
+struct SumArgs;
+
+int Sum(const struct SumArgs *args);
+
+#endif
