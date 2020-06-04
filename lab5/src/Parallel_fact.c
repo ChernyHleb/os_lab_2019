cc8e28479214b0a54600880769f6ab1765bcc36c
chernyhleb <popovarlol@gmail.com>
2020-06-04 13:09:39 +0000

    fixed some bugs

--- lab5/src/Parallel_fact.c
+++ lab5/src/Parallel_fact.c
@@ -5,7 +5,7 @@
 #include <pthread.h>
 #include <getopt.h>
 
-struct somedata
+struct DATA
 {
 	uint64_t* a;
 	uint64_t s, e;
@@ -16,30 +16,32 @@ pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 uint64_t shared;
 
 void *ThreadSum(void *args) {
-  struct somedata *smd = (struct somedata *)args;
-	for(uint64_t i = smd->s; i < smd->e; i++)
+  struct DATA *SharedData = (struct DATA *)args;
+  uint32_t i;
+	for(i = SharedData->s; i < SharedData->e; i++)
 	{
 		pthread_mutex_lock(&mtx);
-		*(smd->a) = (*(smd->a)*i)%smd->m;
+		*(SharedData -> a) = ( *(SharedData -> a) * i) % SharedData -> m;
 		pthread_mutex_unlock(&mtx);
 	}
   return 0;
 }
 
 int main(int argc, char **argv) {
-	shared = 1;
-  uint32_t k = -1;
-  uint32_t pnum = -1;
-  uint32_t mod = -1;
-        static struct option options[] = 	{{"k", required_argument, 0, 0},
-                                                        {"pnum", required_argument, 0, 0},
-                                                        {"mod", required_argument, 0, 0},
-                                                        {0, 0, 0, 0}};
+    shared = 1;
+    uint32_t k = -1;
+    uint32_t pnum = -1;
+    uint32_t mod = -1;
+    static struct option options[] = 	{{"k", required_argument, 0, 0},
+                                         {"pnum", required_argument, 0, 0},
+                                         {"mod", required_argument, 0, 0},
+                                         {0, 0, 0, 0}
+                                        };
 
-        int option_index = 0;
+    int option_index = 0;
 	while(1)
 	{
-	        int c = getopt_long(argc, argv, "f", options, &option_index);
+	    int c = getopt_long(argc, argv, "f", options, &option_index);
 		if (c == -1) break;
 		switch(c)
 		{
@@ -69,46 +71,46 @@ int main(int argc, char **argv) {
 	}
         if (optind < argc) 
         {
-                printf("Has at least one no option argument\n");
-                return 1;
+            printf("Has at least one no option argument\n");
+            return 1;
         }
         if (k == -1 || pnum == -1 || mod == -1) 
         {
-                printf("Usage: %s --k \"num\" --mod \"num\" --mod \"num\" \n", argv[0]);
-                return 1;
+            printf("Usage: %s --k \"num\" --pnum \"num\" --mod \"num\" \n", argv[0]);
+            return 1;
         }
-  pthread_t threads[pnum];
-	struct somedata smd[pnum];
-	for(uint64_t i = 0; i < pnum; i++)
+    pthread_t threads[pnum];
+	struct DATA SharedData[pnum];
+    uint32_t i;
+	for(i = 0; i < pnum; i++)
 	{
-		smd[i].a = &shared;
-		smd[i].s = i*k/pnum + 1;
-		smd[i].e = (i == (pnum - 1)) ? k + 1 : (i+1)*k/pnum + 1;
-		smd[i].m = mod;
+		SharedData[i].a = &shared;
+		SharedData[i].s = i*k/pnum + 1;
+		SharedData[i].e = (i == (pnum - 1)) ? k + 1 : (i+1)*k/pnum + 1;
+		SharedData[i].m = mod;
 	}
 
-  struct timeval start_time;
-  gettimeofday(&start_time, NULL);
+    struct timeval start_time;
+    gettimeofday(&start_time, NULL);
 
-  for (uint32_t i = 0; i < pnum; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&smd[i])) {
-      printf("Error: pthread_create failed!\n");
-      return 1;
+    for (i = 0; i < pnum; i++) {
+        if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&SharedData[i])) {
+            printf("Error: pthread_create failed!\n");
+            return 1;
+        }
     }
-  }
 
-  for (uint32_t i = 0; i < pnum; i++) {
-    pthread_join(threads[i], NULL);
-  }
+    for (i = 0; i < pnum; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
-        struct timeval finish_time;
-        gettimeofday(&finish_time, NULL);
+    struct timeval finish_time;
+    gettimeofday(&finish_time, NULL);
 
-        double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
-        elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
+    double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
+    elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
-
-  printf("Result: %d\n", shared);
-        printf("Elapsed time: %fms\n", elapsed_time);
-  return 0;
+    printf("Result: %ld\n", shared);
+    printf("Elapsed time: %fms\n", elapsed_time);
+    return 0;
 }
